5d907ffc3b19a3fcd591dd2acee6dcbea1c1d046
Steve Withington <steve.withington@blueriver.com>
2019-02-25 23:40:42 -0500

    working on auto bundle

--- mura-docker/bundle.sh
+++
@@ -1,4 +0,0 @@
-echo "Downloading Mura Training bundle..."
-
-#curl --location 'https://github.com/blueriver/MuraBootstrap4Bundle/raw/master/MuraBundle.zip' -o /tmp/MuraBundle.zip
-curl --location 'https://github.com/stevewithington/mura-training/blob/master/mura-docker/www/tmp/MuraBundle.zip?raw=true' -o /tmp/MuraBundle.zip
--- mura-docker/www/training/index.cfm
+++ mura-docker/www/training/index.cfm
@@ -1,4 +1,1 @@
-<cfdump var="#ExpandPath("/MuraWRM")#"/>
-<!--- <cfdump var="#DirectoryExists("/MuraWRM/tmp")#"/> --->
-<cfdump var="#FileExists("/MuraWRM/tmp/MuraBundle.zip")#"/>
-<!--- <cfdump var="#FileExists("/MuraWRM/tmp/MuraBundle.zip")#"/> --->
+<cfdump var="#FileExists("/tmp/MuraBundle.zip")#"/>
