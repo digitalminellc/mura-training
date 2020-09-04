193e988af04cfc11b232b93d1420ecaae912113b
stevewithington <steve@digitalmine.com>
2020-09-04 11:21:02 -0500

    fix bundle

--- build/dependencies.sh
+++ build/dependencies.sh
@@ -2,4 +2,4 @@ echo "Downloading bundle ..."
 
 echo "$CFENGINE"
 
-curl --location 'https://github.com/digitalminellc/mura-bundle-bootstrap4/blob/master/MuraBundle.zip?raw=true' -o /tmp/MuraBundle.zip
+curl --location 'https://github.com/digitalminellc/mura-bundle-bootstrap4/archive/master.zip' -o /tmp/MuraBundle.zip
